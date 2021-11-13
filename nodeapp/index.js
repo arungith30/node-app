403de8a163f539eced7849111081cd4f61a4621f
Arun kumar s <arunks002info@gmail.com>
2021-11-13 23:02:30 +0530

    change

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const {MongoClient}=require("mongodb")
-import dotenv from 'dotenv'
-const app=express();
+import express from "express";
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+const app = express();
 const PORT=process.env.PORT;
 
 async function createConnection(){
-const MONGO_URL=process.env.mongo_uri 
-const client =new MongoClient(MONGO_URL);
+const MONGO_URL=process.env.mongo_uri ;
+const client = new MongoClient(MONGO_URL);
 
 try{
     await client.connect();
-    return client
-    //const result=await client.db("contestants").collection("poll").findOne({id:"1"});
+    return client;
+    
     console.log("successfully connected",result);
 }catch(err){
     console.log(err);
@@ -21,16 +21,16 @@ app.get("/",(request,response)=>{
 response.send("Welcome to my node app");
 });
 app.get("/poll/:id",async(request,response)=>{
-const id = request.params.id;
-const client =await createConnection();
-const contestant =await getPollById(client,id);
 
-response.send(contestant);
+        const id = request.params.id;
+        const client =await createConnection();
+        const contestant =await getPollById(client,id);
+        response.send(contestant);
 });
 app.listen(PORT,()=>console.log("The server is started"));
 
 async function getPollById(client,id){
     const result =await client.db("contestants").collection("poll").findOne({id: id });
-    console.log("successfully connected",result);
+    console.log("successfully connected",results);
     return result;
 }
